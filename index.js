affaab0566575305a7dc6838bf7ca39649a0adf0
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-08-09 22:44:31 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const arr=[];
+function writeCards(arr,event){
+const arr2=[];
+for(let i=0; i<arr.length; i++){
+    arr2.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+}
+return arr2;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number){
+    while(number>0 || number===0){
+        console.log(number);
+        number--;
+    }
+}
+countDown(10);
